755c7212a0e731f8ee5b19dc9c92ecf828e1312f
jomeiii <136589834+jomeiii@users.noreply.github.com>
2024-09-25 19:39:06 +0500

    Edit: builds

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 ﻿TOKEN = "7711227713:AAHCqSzdCirETZ1fOnOMJ0bwuUg8x72Y9bI"
 SHORT_NAMEGAME = "WebApp"
-GAME_URL = "https://jomeiii.github.io/TonTelegramWebAppDeploy"
+GAME_URL = "https://jomeiii.github.io/TonWebApp/"
